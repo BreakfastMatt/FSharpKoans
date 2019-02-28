f508f9d0820f2500c8600a162f2126826cc98475
Matthew Lewis <G17L3581@ict.ru.ac.za>
2019-02-28 17:11:27 +0200

    Finished test 1 in the AboutIteration class

--- FSharpKoans/AboutIteration.fs
+++ FSharpKoans/AboutIteration.fs
@@ -9,25 +9,25 @@ module ``05: To iterate is human; to recurse, divine`` =
 
     [<Test>]
     let ``01 `rec` exposes the name of the function for use inside the function`` () =
-        let rec converge d c n =
+        let rec converge d c n = 
             match d = c with
             | false ->
                 match d < c with
-                | true -> converge (d+10) c (n+1)
+                | true -> converge (d+10) c (n+1) //if you removed the rec keyword above, you would get: The value or constructor 'converge' is not defined.
                 | false -> converge (d - 1) c (n+1)
             | true -> n
-        converge 3 10  |> should equal __
+        converge 3 10 0 |> should equal 4 //just work through the function with good ol' pen & paper :)
 
     [<Test>]
     let ``02 Tail recursion stops a stack overflow from occurring`` () =
         // CHANGE the recursive function to be tail recursive.
-        let myfun n =
+        let myfun n = 
             let sq = n*n
             let v = sq*sq*sq*sq
             let rec inner count =
-                match count = v with
+                match count = v with 
                 | true -> 0
                 | false -> -1 + inner (count+1)
             inner sq
 
-        myfun 12 |> should equal
+        myfun 12 |> should equal 15
